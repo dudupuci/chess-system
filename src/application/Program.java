ebde6e0bce40f32b557ea0c5f565e6c974f42bfb
Eduardo Pucinelli <dudupucinelli@gmail.com>
2022-02-17 00:54:05 -0300

    Adding new Pieces on ChessMatch

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,19 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Position pos = new Position(3, 5);
+		//System.out.println(pos);
+		
+		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
 
 	}
 
